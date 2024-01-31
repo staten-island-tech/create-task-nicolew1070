003e5a1eb13dd54d5fe69506eaefc97175dbe64d
nicolew88 <nicolew88@nycstudents.net>
2024-01-31 13:15:10 -0500

    .

--- project/js/main.js
+++ project/js/main.js
@@ -4,20 +4,50 @@ const num =[1,2,3,4,5,6,7,8,9,10];
 
 DOMSelectors.enter.addEventListener("click", function (event) {
     event.preventDefault
-    guessing();
+    result();
+    guesses ();
 });
+
+function clearFields () {
+    DOMSelectors.search.value = "";
+}
+
+function fields () {
+    DOMSelectors.guess.textContent = "";
+}
 
 let answer = num[(Math.floor(Math.random()*num.length))]
 console.log(answer);
+//function does not work -> no number is generated + answer not defined in the function 'result'
 
-function guessing () {
+function result () {
     let search = DOMSelectors.search.value
     if (search < answer) {
-        DOMSelectors.guess.textContent = "Too Low"; //does not work
+        DOMSelectors.guess.textContent = "Too Low"; 
+        clearFields();
     } else if (search > answer) {
         DOMSelectors.guess.textContent = "Too High";
+        clearFields();
     } else 
         DOMSelectors.guess.textContent = "Correct";
+        clearFields();
 };
 
+DOMSelectors.restart.addEventListener("click", function (event) {
+    event.preventDefault
+    again ();
+    fields ();
+});
+
+let guess = 1; //does not work
+
+function guesses () {
+    if (guess === 3) {
+    DOMSelectors.guess.textContent= "You're Wrong";
+}};
+guess++;
+
 //need to work on restarting the game 
+//need to work on limiting to 3 tries
+//need to work on amount of times user has won 
+//do css
--- project/js/num.js
+++ project/js/num.js
@@ -2,6 +2,7 @@ const DOMSelectors = {
     search: document.querySelector("#find"),
     enter: document.querySelector("#enter"),
     guess: document.querySelector(".guess"),
+    restart: document.querySelector("#restart"),
     };
 
 export {DOMSelectors};
